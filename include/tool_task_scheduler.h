e53d53d3c755b6a50a08f67a1a90ae93b25ab8aa
ZxyKira <danny41553@gmail.com>
2021-03-28 23:21:28 +0800

    [feat] while loop to execute
    [feat] while loop to execute
    
    add execute breakExecute
    remove start stop
    refactor tab to space and title

--- include/tool_task_scheduler.h
+++ include/tool_task_scheduler.h
@@ -15,73 +15,71 @@ extern "C" {
 #endif
 
 //-------------------------------------
-//	Typedef 
+//  Typedef 
 //------------------------------------- 
-typedef void (*tool_ts_execute)(void* args);
+typedef void (*tool_ts_execute_t)(void* args);
 
-//-------------------------------------
-//	Struct tool_ts_event_t 
-//------------------------------------- 
-typedef struct{
-	tool_ts_execute execute;
-	void*	attachment;
+/* *****************************************************************************************
+ *  Struct tool_ts_event_t
+ */
+typedef struct _tool_ts_event_t{
+  tool_ts_execute_t execute;
+  void*  attachment;
 }tool_ts_event_t;
 
-//-------------------------------------
-//	Struct tool_ts_handle_t
-//------------------------------------- 
+/* *****************************************************************************************
+ *  Struct tool_ts_handle_t
+ */
 typedef struct _tool_ts_handle_t{
-	struct{
-		tool_fifo_t high;
-		tool_fifo_t normal;
-		tool_fifo_t low;
-	}fifo;
-	struct{
-		tool_ts_event_t idle;
-		tool_ts_event_t reserved;
-	}execute;
+  struct{
+    tool_fifo_t high;
+    tool_fifo_t normal;
+    tool_fifo_t low;
+  }fifo;
   uint32_t flag;
-	tool_ts_event_t idleCallback;
 }tool_ts_handle_t;
 
-//-------------------------------------
-//	Struct tool_ts_handle_t
-//------------------------------------- 
+/* *****************************************************************************************
+ *  Enum tool_ts_prtority
+ */
 typedef enum{
-	tool_ts_prtority_low,
-	tool_ts_prtority_normal,
-	tool_ts_prtority_high
+  tool_ts_prtority_low,
+  tool_ts_prtority_normal,
+  tool_ts_prtority_high
 }tool_ts_prtority;
 
-//-------------------------------------
-//	Struct tool_ts_config_buffer_t
-//------------------------------------- 
+/* *****************************************************************************************
+ *  Struct tool_ts_config_buffer_t
+ */
 typedef struct _tool_ts_config_buffer_t{
-	tool_ts_event_t *eventBuffer;
-	uint16_t bufferQuantity;
+  tool_ts_event_t *eventBuffer;
+  uint16_t bufferQuantity;
 }tool_ts_config_buffer_t;
 
-//-------------------------------------
-//	Struct tool_ts_config_t
-//------------------------------------- 
+/* *****************************************************************************************
+ *  Struct tool_ts_config_t
+ */
 typedef struct{
-	tool_ts_config_buffer_t prtorityLow;
-	tool_ts_config_buffer_t prtorityNormal;
-	tool_ts_config_buffer_t prtorityHigh;
-	tool_ts_event_t idleCallback;
+  tool_ts_config_buffer_t prtorityLow;
+  tool_ts_config_buffer_t prtorityNormal;
+  tool_ts_config_buffer_t prtorityHigh;
+  tool_ts_event_t idleCallback;
 }tool_ts_config_t;
 
-//-------------------------------------
-//	Struct tool_ts_API_T
-//------------------------------------- 
+/* *****************************************************************************************
+ *  Struct tool_ts_handle_t
+ */
 typedef struct _tool_ts_api_t{
-	bool(*initialze)    (tool_ts_handle_t *handle, const tool_ts_config_t *config);
-	bool(*start)        (tool_ts_handle_t *handle);
-	bool(*stop)         (tool_ts_handle_t *handle);
-	bool(*addTask)      (tool_ts_handle_t *handle, tool_ts_execute execute, void* attachment, tool_ts_prtority prtority);
-	bool(*addTaskSuper) (tool_ts_handle_t *handle, tool_ts_execute execute, void* attachment, tool_ts_prtority prtority);	
+  bool(*initialze)    (tool_ts_handle_t *handle, const tool_ts_config_t *config);
+  bool(*execute)      (tool_ts_handle_t *handle);
+  bool(*breakExecute) (tool_ts_handle_t *handle);
+  bool(*addTask)      (tool_ts_handle_t *handle, tool_ts_execute_t execute, void* attachment, tool_ts_prtority prtority);
+  bool(*addTaskSuper) (tool_ts_handle_t *handle, tool_ts_execute_t execute, void* attachment, tool_ts_prtority prtority);  
 }tool_ts_api_t;
 
+/* *****************************************************************************************
+ *  Extern API
+ */
 extern const tool_ts_api_t tool_ts_api;
 
 #ifdef __cplusplus
@@ -89,5 +87,5 @@ extern const tool_ts_api_t tool_ts_api;
 #endif
 #endif
 /* *****************************************************************************************
- *	End of file
+ *  End of file
  */
--- source/tool_task_scheduler.c
+++ source/tool_task_scheduler.c
@@ -9,121 +9,134 @@
 #include "tool_fifo.h"
 
 /* *****************************************************************************************
- *	Typedef Struct
+ *  Typedef Struct
  */
-static void tool_ts_idleExecute(void* args){}
 
 /* *****************************************************************************************
- *	Public Function 
+ *  Public Function 
  */
+ 
+/*-------------------------------------
+ *  tool_ts_initialze 
+ */ 
 static bool tool_ts_initialze(tool_ts_handle_t* handle, const tool_ts_config_t *config) {
-	tool_fifo_config_t fifo_cfg;
-		
-	if (config->idleCallback.execute == 0x00000000)
-		handle->idleCallback.execute = tool_ts_idleExecute;
-	else
-    handle->idleCallback = config->idleCallback;
-	
-	
+  tool_fifo_config_t fifo_cfg;
+  
   fifo_cfg.buffer = &config->prtorityHigh.eventBuffer[0];
-	fifo_cfg.count = config->prtorityHigh.bufferQuantity;
-	fifo_cfg.itemSize = sizeof(tool_ts_event_t);
-	tool_fifo_init(&handle->fifo.high, &fifo_cfg);
-	
+  fifo_cfg.count = config->prtorityHigh.bufferQuantity;
+  fifo_cfg.itemSize = sizeof(tool_ts_event_t);
+  tool_fifo_init(&handle->fifo.high, &fifo_cfg);
+  
   fifo_cfg.buffer = &config->prtorityNormal.eventBuffer[0];
-	fifo_cfg.count = config->prtorityNormal.bufferQuantity;
-	fifo_cfg.itemSize = sizeof(tool_ts_event_t);
-	tool_fifo_init(&handle->fifo.normal, &fifo_cfg);
-	
+  fifo_cfg.count = config->prtorityNormal.bufferQuantity;
+  fifo_cfg.itemSize = sizeof(tool_ts_event_t);
+  tool_fifo_init(&handle->fifo.normal, &fifo_cfg);
+  
   fifo_cfg.buffer = &config->prtorityLow.eventBuffer[0];
-	fifo_cfg.count = config->prtorityLow.bufferQuantity;
-	fifo_cfg.itemSize = sizeof(tool_ts_event_t);
-	tool_fifo_init(&handle->fifo.low, &fifo_cfg);
-
-	handle->flag = 0;
-	return true;
+  fifo_cfg.count = config->prtorityLow.bufferQuantity;
+  fifo_cfg.itemSize = sizeof(tool_ts_event_t);
+  tool_fifo_init(&handle->fifo.low, &fifo_cfg);
+  
+  handle->flag = 0;
+  return true;
 }
 
-static bool tool_ts_start(tool_ts_handle_t* handle) {
-	tool_ts_event_t task = {0x00000000, 0x00000000};
+/*-------------------------------------
+ *  tool_ts_execute 
+ */ 
+static bool tool_ts_execute(tool_ts_handle_t* handle) {
+  tool_ts_event_t task = {0x00000000, 0x00000000};
 
-	if (handle->flag != 0)
-		return false;
-
-	handle->flag = 1;
-
-	while (handle->flag != 0) {
-		if (tool_fifo_pop(&handle->fifo.high, &task)) 
-			task.execute(task.attachment);
-		else if (tool_fifo_pop(&handle->fifo.normal, &task))
-			task.execute(task.attachment);
-		else if (tool_fifo_pop(&handle->fifo.low, &task))
-			task.execute(task.attachment);
-		else
-			handle->idleCallback.execute(handle->idleCallback.attachment);
-	}
-	return true;
+  if(handle->flag)
+    return false;
+  
+  handle->flag = 1;
+  
+  while(handle->flag) {
+    if (tool_fifo_pop(&handle->fifo.high, &task)) 
+      task.execute(task.attachment);
+    else if (tool_fifo_pop(&handle->fifo.normal, &task))
+      task.execute(task.attachment);
+    else if (tool_fifo_pop(&handle->fifo.low, &task))
+      task.execute(task.attachment);
+    else
+      break;
+  }
+  
+  handle->flag = 0;
+  return true;
 }
 
-static bool tool_ts_stop(tool_ts_handle_t* handle) {
-	if (handle->flag == 0)
-		return false;
+/*-------------------------------------
+ *  tool_ts_breakExecute 
+ */ 
+static bool tool_ts_breakExecute(tool_ts_handle_t* handle) {
+  if (!handle->flag)
+    return false;
 
-	handle->flag = 0;
-	return true;
+  handle->flag = 0;
+  return true;
 }
 
-
-static bool tool_ts_addTask(tool_ts_handle_t* handle, tool_ts_execute execute, void* attachment, tool_ts_prtority prtority) {
-	if(!execute)
-	  return false;
-	
-  tool_ts_event_t task = {
-	  .execute = execute,
-		.attachment = attachment
-	};
-	
-	switch (prtority) {
-		case tool_ts_prtority_low:
-			return tool_fifo_insert(&handle->fifo.low, &task);
-		case tool_ts_prtority_normal:
-			return tool_fifo_insert(&handle->fifo.normal, &task);
-		case tool_ts_prtority_high:
-			return tool_fifo_insert(&handle->fifo.high, &task);
-		default:
-			return false;
-	}
-}
-
-static bool tool_ts_addTaskSuper(tool_ts_handle_t* handle, tool_ts_execute execute, void* attachment, tool_ts_prtority prtority) {
+/*-------------------------------------
+ *  tool_ts_addTask 
+ */ 
+static bool tool_ts_addTask(tool_ts_handle_t* handle, tool_ts_execute_t execute, void* attachment, tool_ts_prtority prtority) {
   if(!execute)
-	  return false;
-	
-	tool_ts_event_t task = {
-	  .execute = execute,
-		.attachment = attachment
-	};
-		
-	switch (prtority) {
-		case tool_ts_prtority_low:
-			return tool_fifo_insertTail(&handle->fifo.low, &task);
-		case tool_ts_prtority_normal:
-			return tool_fifo_insertTail(&handle->fifo.normal, &task);
-		case tool_ts_prtority_high:
-			return tool_fifo_insertTail(&handle->fifo.high, &task);
-		default:
-			return false;
-	}
+    return false;
+  
+  tool_ts_event_t task = {
+    .execute = execute,
+    .attachment = attachment
+  };
+  
+  switch (prtority) {
+    case tool_ts_prtority_low:
+      return tool_fifo_insert(&handle->fifo.low, &task);
+    case tool_ts_prtority_normal:
+      return tool_fifo_insert(&handle->fifo.normal, &task);
+    case tool_ts_prtority_high:
+      return tool_fifo_insert(&handle->fifo.high, &task);
+    default:
+      return false;
+  }
 }
 
+/*-------------------------------------
+ *  tool_ts_addTaskSuper 
+ */ 
+static bool tool_ts_addTaskSuper(tool_ts_handle_t* handle, tool_ts_execute_t execute, void* attachment, tool_ts_prtority prtority) {
+  if(!execute)
+    return false;
+  
+  tool_ts_event_t task = {
+    .execute = execute,
+    .attachment = attachment
+  };
+    
+  switch (prtority) {
+    case tool_ts_prtority_low:
+      return tool_fifo_insertTail(&handle->fifo.low, &task);
+    case tool_ts_prtority_normal:
+      return tool_fifo_insertTail(&handle->fifo.normal, &task);
+    case tool_ts_prtority_high:
+      return tool_fifo_insertTail(&handle->fifo.high, &task);
+    default:
+      return false;
+  }
+}
+
+/* *****************************************************************************************
+ *  Link API 
+ */
 const tool_ts_api_t tool_ts_api = {
-	.initialze = tool_ts_initialze,
-	.start = tool_ts_start,
-	.stop = tool_ts_stop,
-	.addTask = tool_ts_addTask,
-	.addTaskSuper = tool_ts_addTaskSuper
+  .initialze    = tool_ts_initialze,
+  .execute      = tool_ts_execute,
+  .breakExecute = tool_ts_breakExecute,
+  .addTask      = tool_ts_addTask,
+  .addTaskSuper = tool_ts_addTaskSuper
 };
 
 /* *****************************************************************************************
- *	End of file
+ *  End of file
  */
